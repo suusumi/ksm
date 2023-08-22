58c165e491f5cf90793a9aa4f130f479df6104e5
gogenzollern <arseny.lomakin@gmail.com>
2023-08-23 02:10:39 +0300

    [database] Migrate infographics

---
+++ server/clinic_server/prisma/migrations/20230822230755_infographics_added/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Infographics" (
+    "id" SERIAL NOT NULL,
+    "value" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Infographics_pkey" PRIMARY KEY ("id")
+);
