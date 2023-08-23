51d8c25bdb11301f6c0e299fb4c0d6e3b67e7f25
gogenzollern <arseny.lomakin@gmail.com>
2023-08-24 01:55:51 +0300

    [server] database module connected to nest app

--- server/clinic_server/dist/app.module.js
+++ server/clinic_server/dist/app.module.js
@@ -10,11 +10,12 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const database_module_1 = require("./database/database.module");
 let AppModule = exports.AppModule = class AppModule {
 };
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [database_module_1.DatabaseModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- server/clinic_server/dist/main.js
+++ server/clinic_server/dist/main.js
@@ -5,6 +5,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(8080);
+    app.enableShutdownHooks();
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
