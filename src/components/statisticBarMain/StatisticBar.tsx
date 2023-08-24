131a57133d5bef84cb8c86d6b8ddfe8fe1fc1266
Sergey <79329868+GhostGrand@users.noreply.github.com>
2023-08-24 13:11:17 +0300

    Написана заглушка под сервер для StaticBar
    Также поправлены стили

--- src/components/statisticBarMain/StatisticBar.tsx
+++ src/components/statisticBarMain/StatisticBar.tsx
@@ -1,8 +1,33 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography';
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+interface StatisticItem {
+  title: string;
+  description: string;
+}
 
 function StatisticBar() {
+  const [data, setData] = useState<StatisticItem[]>([]);
+
+  useEffect(() => {
+    // Пример получения данных с сервера:
+    // fetch('URL_ВАШЕГО_СЕРВЕРА')
+    //   .then(response => response.json())
+    //   .then(data => setData(data))
+    //   .catch(error => console.error('Ошибка при получении данных', error));
+
+    const statisticData: StatisticItem[] = [
+      { title: '35+', description: 'Специалистов из разных областей' },
+      { title: '17 лет', description: 'На страже вашего здоровья' },
+      { title: '18+', description: 'Областей медицинской помощи' },
+    ];
+
+    setData(statisticData);
+  }, []);
+
+
+
   const TitleText = {
     color: '#288e81',
     textDecoration: 'none',
@@ -21,30 +46,12 @@ function StatisticBar() {
   return (
     <div>
       <Grid container spacing={5}>
-        <Grid item xs={12} sm={4} md={4}>
-          <Typography sx={TitleText}>
-            35+
-          </Typography>
-          <Typography sx={StandartText}>
-            Специалистов из разных областей
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={4} md={4}>
-          <Typography sx={TitleText}>
-            17 лет
-          </Typography>
-          <Typography sx={StandartText}>
-            На страже вашего здоровья
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={4} md={4}>
-          <Typography sx={TitleText}>
-            18+
-          </Typography>
-          <Typography sx={StandartText}>
-            Областей медицинской помощи
-          </Typography>
-        </Grid>
+        {data.map((item, index) => (
+          <Grid key={index} item xs={12} sm={4} md={4} style={{ marginBottom: '30px' }}>
+            <Typography sx={TitleText}>{item.title}</Typography>
+            <Typography sx={StandartText}>{item.description}</Typography>
+          </Grid>
+        ))}
       </Grid>
     </div>
   )
