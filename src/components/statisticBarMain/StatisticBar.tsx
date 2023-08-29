095474677f7be45523408935f7761ecfc6dc5d8d
Sergey <79329868+GhostGrand@users.noreply.github.com>
2023-08-29 13:51:57 +0300

    [style](statistic bar): fixed styles for adaptive

--- src/components/statisticBarMain/StatisticBar.tsx
+++ src/components/statisticBarMain/StatisticBar.tsx
@@ -1,6 +1,9 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography';
 import React, { useState, useEffect } from 'react'
+import { ThemeProvider, useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Box from '@mui/material/Box';
 
 interface StatisticItem {
   title: string;
@@ -26,30 +29,42 @@ function StatisticBar() {
     setData(statisticData);
   }, []);
 
-
+  const theme = useTheme();
+  const isXsScreen = useMediaQuery(theme.breakpoints.only('xs'));
 
   const TitleText = {
     color: '#288e81',
     textDecoration: 'none',
-    fontSize: 65,
-    // TODO: шрифт Austin Normal, возможно Regular
-    fontFamily: 'AustinMedium, sans-serif'
+    fontSize: isXsScreen ? 35 : 65,
+    fontFamily: 'AustinMedium, sans-serif',
+    display: 'flex',
+    textAlign: isXsScreen ? 'center' : 'left',
   };
 
   const StandartText = {
     color: 'black',
     textDecoration: 'none',
-    fontSize: 22,
+    fontSize: isXsScreen ? 16 : 22,
     fontFamily: 'PT-Sans, sans-serif',
-    maxWidth: '250px'
+    maxWidth: '250px',
+    display: 'flex',
+    textAlign: isXsScreen ? 'center' : 'left',
   };
   return (
-    <div>
+    <div style={{padding: '20px 0px'}}>
       <Grid container spacing={5}>
         {data.map((item, index) => (
           <Grid key={index} item xs={12} sm={4} md={4} style={{ marginBottom: '30px' }}>
-            <Typography sx={TitleText}>{item.title}</Typography>
-            <Typography sx={StandartText}>{item.description}</Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: isXsScreen ? 'center' : 'flex-start', // Центрирование по центру на xs, влево на остальных
+              }}
+            >
+              <Typography sx={TitleText}>{item.title}</Typography>
+              <Typography sx={StandartText}>{item.description}</Typography>
+            </Box>
           </Grid>
         ))}
       </Grid>
