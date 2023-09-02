1bad7057ddfb1eff2cc27e424627d0efaa69d891
andreypisarev <xaandreynx@gmail.com>
2023-09-02 16:42:06 +0300

    [feat]<navbar>: create navbar for admin panel

---
+++ src/components/header/HeaderAdmin.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import logo from "../../assets/content/header-logo.svg";
+import PrimaryButton from "../primaryButton/PrimaryButton";
+import theme from "../../assets/theme/Theme";
+
+const styles = {
+    buttonExit: {
+        color: 'white',
+        backgroundColor: theme.palette.primary.main,
+        fonstSize: '14px',
+        borderRadius: '30px',
+        textTransform: 'none',
+        padding: '8px 26px',
+        whiteSpace: 'nowrap',
+    },
+    button: {
+        color: 'black',
+        fontSize: '14px',
+        textTransform: 'none',
+        padding: '8px 16px',
+        whiteSpace: 'nowrap',
+    }
+}
+
+export const HeaderAdmin = () => {
+    return(
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static" sx={{ paddingY: '10px', borderRadius: '0 0 20px 20px', backgroundColor: 'white', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)'}}>
+                <Toolbar>
+                    <Typography component="div" sx={{ flexGrow: 1 }}>
+                        <img src={logo} alt="Логотип" style={{ width: '30%'}}/>
+                    </Typography>
+
+                    <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center' }}>
+                        <Button color="inherit" sx={styles.button}>Инфографика</Button>
+                        <Button color="inherit" sx={styles.button}>Баннеры</Button>
+                        <Button color="inherit" sx={styles.button}>Специалисты</Button>
+                        <Button color="inherit" sx={styles.button}>Услуги</Button>
+                    </Box>
+
+                    <Button color="inherit" href="/" sx={styles.buttonExit}>ВЫЙТИ</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
