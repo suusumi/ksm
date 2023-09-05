0e7605b1a6c97a5698eeedfda7171fbcabf36c01
andreypisarev <xaandreynx@gmail.com>
2023-09-05 15:43:49 +0300

    [refactor]: correct admin panel container

--- src/containers/AdminPanelContainer.tsx
+++ src/containers/AdminPanelContainer.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { HeaderAdmin } from "../components/header/HeaderAdmin";
-import { Grid } from "@mui/material";
+import { Container } from "@mui/material";
 
 interface IAdminPanelContainer {
     children: JSX.Element
 }
 
 export const AdminPanelContainer: React.FC<IAdminPanelContainer> = ({ children }) => {
-    return (<Grid container direction={'column'} spacing={5}>
-        <Grid item>
-            <HeaderAdmin />
-        </Grid>
-
-        <Grid item>
-            {children}
-        </Grid>
-    </Grid>
+    return (
+    <Container>
+        <HeaderAdmin/>
+        {children}
+    </Container>
     );
 }
