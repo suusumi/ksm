d8d2e0309323fb3b744682536519a5532c96e23f
gogenzollern <arseny.lomakin@gmail.com>
2023-09-12 22:55:40 +0300

    [feat] (server): Added opportuninty to upload image and json together when creating banner

--- server/clinic_server/src/banners/banners.controller.ts
+++ server/clinic_server/src/banners/banners.controller.ts
@@ -8,11 +8,15 @@ import {
   Delete,
   UsePipes,
   ValidationPipe,
-  ParseIntPipe
+  ParseIntPipe, UseInterceptors, UploadedFile
 } from '@nestjs/common';
 import { BannersService } from './banners.service';
 import { CreateBannerDto } from './dto/create-banner.dto';
 import { UpdateBannerDto } from './dto/update-banner.dto';
+import {FileInterceptor} from "@nestjs/platform-express";
+import {diskStorage} from "multer";
+import {extname} from "path";
+import {CreateSpecialistDto} from "../specialists/dto/create-specialist.dto";
 
 @Controller('banners')
 export class BannersController {
@@ -43,5 +47,35 @@ export class BannersController {
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number) {
     return await this.bannersService.remove(+id);
+  }
+
+  @Post('createwithimage')
+  @UseInterceptors(FileInterceptor('image', {
+    storage: diskStorage({
+      destination: './public/uploads/banners', // Путь к папке для сохранения изображений
+      filename: (req, file, cb) => {
+        const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+        return cb(null, `${randomName}${extname(file.originalname)}`);
+      },
+    }),
+  }))
+
+  //@UsePipes(new ValidationPipe())
+  async createSpecialistWithImage(
+      @UploadedFile() file: Express.Multer.File,
+      @Body() bodyData: CreateBannerDto
+  ) {
+    // Внимание, костыли!
+    // Преобразую то, что пришло, в строку
+    const jsonString = JSON.stringify(bodyData);
+    // Преобразую в JSON пришедшую строку
+    const parsedObject = JSON.parse(jsonString);
+    // Получаю строку с данными из объекта json
+    const bodyDataString = parsedObject.bodyData;
+    // Создаю DTO на основе строки с данными из объекта json
+    const createBannerDto: CreateBannerDto = JSON.parse(bodyDataString);
+    // Заменяю свойство photo_path на файл, который upload-им
+    createBannerDto.img_path = file.path;
+    return await this.bannersService.create(createBannerDto);
   }
 }
