155766f8efedf619fec5a12dc0ac0158ab9b09d4
gogenzollern <arseny.lomakin@gmail.com>
2023-09-15 21:38:06 +0300

    [refactor] (server) Добавлены комментарии

--- server/clinic_server/src/main.ts
+++ server/clinic_server/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Обход безопасности CORS
   app.enableCors({
     origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
