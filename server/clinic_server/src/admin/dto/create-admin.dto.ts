4f680aaeceec8e1b7372e5f55ee269920e41aae2
gogenzollern <arseny.lomakin@gmail.com>
2023-09-17 19:58:58 +0300

    [feat] (server) dto для администраторов создано

--- server/clinic_server/src/admin/dto/create-admin.dto.ts
+++ server/clinic_server/src/admin/dto/create-admin.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAdminDto {}
+import {IsOptional, IsString} from "class-validator";
+
+export class CreateAdminDto {
+
+    @IsString()
+    login: string;
+
+    @IsString()
+    password_hash: string;
+
+    @IsOptional()
+    @IsString()
+    full_name: string;
+}
