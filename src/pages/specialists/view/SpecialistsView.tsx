35722de37fc6b358868536bfa453579262505153
andreypisarev <xaandreynx@gmail.com>
2023-09-19 02:24:04 +0300

    [feat]<specialists>: add update specialist method on view

--- src/pages/specialists/view/SpecialistsView.tsx
+++ src/pages/specialists/view/SpecialistsView.tsx
@@ -1,13 +1,16 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, TextField, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"
 import "./../utils/SpecialistsCss.css"
 import theme from "../../../assets/theme/Theme";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ISpecialistsView } from "../model/SpecialistsModel";
 import { IMAGE_URL } from "../../../utils/constants/url.constants";
 import { DialogCreateSpecialists } from "../../../components/dialogs/DialogCreateSpecialist";
+import { DialogUpdateSpecialists } from "../../../components/dialogs/DialogUpdateSpecialists";
+import { SpecialistDto, UpdateSpecialistDto } from "../../../api/specialists/dto";
+import { updateDataSpecialist, updatePhotoSpecialist } from "../../../api/specialists/request";
 
 const styles = {
     TitleText: {
@@ -70,15 +73,15 @@ export const SpecialistsView: React.FC<ISpecialistsView> = (props) => {
                 </Grid>
 
                 <Grid item>
-                    <Button variant="contained" onClick={props.handleClickOpen}>
+                    <Button variant="contained" onClick={props.handleClickCreateOpen}>
                         Создать нового специалиста
                     </Button>
                     <DialogCreateSpecialists
-                        open={props.open}
-                        handleClickOpen={props.handleClickOpen}
-                        handleClose={props.handleClose}
+                        open={props.createOpen}
+                        handleClickOpen={props.handleClickCreateOpen}
+                        handleClose={props.handleCreateClose}
                         createSpecialist={props.createSpecialist}
-                        handleChangeSpecialistForm={props.handleChangeSpecialistForm}
+                        handleChangeSpecialistForm={props.handleChangeNewSpecialistForm}
                         selectedImage={props.selectedImage}
                         setSelectedImage={props.setSelectedImage}
                         handleCreateSpecialist={props.handleCreateSpecialist}
@@ -90,40 +93,113 @@ export const SpecialistsView: React.FC<ISpecialistsView> = (props) => {
                 <Typography style={styles.TitleText}>КАК БУДУТ ВЫГЛЯДЕТЬ СПЕЦИАЛИСТЫ</Typography>
                 <Slider {...settings}>
                     {props.specialists?.map((doctor, index) => (
-                        <div className="card">
-                            <div
-                                className="card-item"
-                                // style={{ maxWidth: "260px", maxHeight: '346px' }}
-                                key={index}
-                            >
-                                <div className="card-top">
-                                    <img src={IMAGE_URL + doctor.photo_path} alt={doctor.name} width={260} height={346} />
-                                </div>
-                                <div className="card-bottom">
-                                    <Typography sx={{ fontFamily: "PT-Sans-Bold" }}>
-                                        {doctor.name}
-                                    </Typography>
-                                    <Typography variant="subtitle1">{doctor.speciality}</Typography>
-                                    <Typography sx={{ fontFamily: "PT-Sans-Bold" }}>
-                                        {doctor.post}
-                                    </Typography>
-                                    <Typography sx={{ color: "#288E81" }}>
-                                        {doctor.degree}
-                                    </Typography>
-                                    <Button
-                                        onClick={props.handleClickOpen}
-                                    >
-                                        Редактировать
-                                    </Button>
-                                    <Button>
-                                        Удалить
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
+                        <SpecialistItem
+                            index={index}
+                            doctor={doctor}
+                            selectedImage={props.selectedImage}
+                            setSelectedImage={props.setSelectedImage}
+                            setValue={props.setValue}
+                        />
                     ))}
                 </Slider>
             </Grid>
         </div>
     );
+}
+
+interface ISpecialistItem {
+    index: number
+    doctor: SpecialistDto,
+    selectedImage: File | null,
+    setSelectedImage: Function,
+    setValue: Function
+}
+
+const SpecialistItem: React.FC<ISpecialistItem> = (props) => {
+    const [open, setOpen] = useState<boolean>(false);
+    const [updateSpecialist, setUpdateSpecialist] = useState<UpdateSpecialistDto>({
+        name: props.doctor.name,
+        post: props.doctor.post,
+        speciality: props.doctor.speciality,
+        degree: props.doctor.degree
+    });
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        props.setSelectedImage(null);
+        setOpen(false);
+    }
+
+    const handleChangeUpdateSpecialistForm = (event: any, type: string) => {
+        if (type === 'name') {
+            setUpdateSpecialist({ ...updateSpecialist, name: event.target.value });
+        } else if (type === 'post') {
+            setUpdateSpecialist({ ...updateSpecialist, post: event.target.value });
+        } else if (type === 'speciality') {
+            setUpdateSpecialist({ ...updateSpecialist, speciality: event.target.value });
+        } else if (type === 'degree') {
+            setUpdateSpecialist({ ...updateSpecialist, degree: event.target.value });
+        } else {
+            console.error('Что-то пошло не так с сохранением данных о специалисте');
+        }
+    }
+
+    const handleUpdateSpecialist = () => {
+        updateDataSpecialist(props.doctor.id, updateSpecialist);
+        if (props.selectedImage !== null) {
+            const formData = new FormData;
+            formData.append('photo', props.selectedImage);
+            updatePhotoSpecialist(props.doctor.id, formData);
+        } else {
+            console.error('Фотогрфаии нет!');
+        }
+        props.setValue({});
+        handleClose();
+    }
+
+    return (<div className="card">
+        <div
+            className="card-item"
+            key={props.index}
+        >
+            <div className="card-top">
+                <img src={IMAGE_URL + props.doctor.photo_path} alt={props.doctor.name} width={260} height={346} />
+            </div>
+            <div className="card-bottom">
+                <Typography sx={{ fontFamily: "PT-Sans-Bold" }}>
+                    {props.doctor.name}
+                </Typography>
+                <Typography variant="subtitle1">{props.doctor.speciality}</Typography>
+                <Typography sx={{ fontFamily: "PT-Sans-Bold" }}>
+                    {props.doctor.post}
+                </Typography>
+                <Typography sx={{ color: "#288E81" }}>
+                    {props.doctor.degree}
+                </Typography>
+                <Button
+                    onClick={handleClickOpen}
+                >
+                    Редактировать
+                </Button>
+                <DialogUpdateSpecialists
+                    open={open}
+                    handleClickOpen={handleClickOpen}
+                    handleClose={handleClose}
+                    updateSpecialist={updateSpecialist}
+                    handleChangeSpecialistForm={handleChangeUpdateSpecialistForm}
+                    selectedImage={props.selectedImage}
+                    setSelectedImage={props.setSelectedImage}
+                    handleUpdateSpecialist={handleUpdateSpecialist}
+                    photo_path={props.doctor.photo_path}
+                    id={props.doctor.id}
+                />
+                <Button>
+                    Удалить
+                </Button>
+            </div>
+        </div>
+    </div>);
 }
