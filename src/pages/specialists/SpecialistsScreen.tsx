b3819f8ef3f0d25a2d335166e20b4de5b02f8002
andreypisarev <xaandreynx@gmail.com>
2023-09-23 13:49:41 +0300

    [feat]: reverse the closure of the modal window with the data update on the page

--- src/pages/specialists/SpecialistsScreen.tsx
+++ src/pages/specialists/SpecialistsScreen.tsx
@@ -56,8 +56,8 @@ export const SpecialistsScreen = () => {
         }
 
         createSpecialist(formData);
-        setValue({});
         handleCreateClose();
+        setValue({});
     }
 
     useEffect(() => {
