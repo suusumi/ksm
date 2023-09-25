42a9daa39a6b2922bce55f517d0e989e10b2a3aa
suusumi <wer113wer@yandex.ru>
2023-09-25 09:51:21 +0300

    [feat](scroll): scrolling is implemented
    Implemented scrolling by header buttons to the required main block

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,39 @@
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { ThemeProvider, useTheme } from '@mui/material/styles';
-import PrimaryButton from '../primaryButton/PrimaryButton';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { ThemeProvider, useTheme } from "@mui/material/styles";
+import PrimaryButton from "../primaryButton/PrimaryButton";
 
 const ButtonAppBar: React.FC<{}> = () => {
+  const scrollToBlock = (blockId: string) => {
+    const block = document.getElementById(blockId);
+    if (block) {
+      block.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   // Состояния для меню иконки и выпадающего меню
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
   const [menuOpen, setMenuOpen] = useState(false);
 
   // Создаем массив элементов меню
   const menuItems = [
-    { label: 'О клинике', onClick: () => handleMenuClick('О клинике') },
-    { label: 'Наши специалисты', onClick: () => handleMenuClick('Наши специалисты') },
-    { label: 'Услуги', onClick: () => handleMenuClick('Услуги') },
-    { label: 'Где мы находимся', onClick: () => handleMenuClick('Где мы находимся') },
-    { label: 'Контакты', onClick: () => handleMenuClick('Контакты') },
+    { label: "О клинике", onClick: () => scrollToBlock("aboutUsBlock") },
+    {
+      label: "Наши специалисты",
+      onClick: () => scrollToBlock("ourSpecialists"),
+    },
+    { label: "Услуги", onClick: () => scrollToBlock("priceBlock") },
+    { label: "Где мы находимся", onClick: () => scrollToBlock("mapBlock") },
+    { label: "Контакты", onClick: () => scrollToBlock("ourContactsBlock") },
   ];
 
   // Обработчик открытия выпадающего меню
@@ -50,33 +60,45 @@ const ButtonAppBar: React.FC<{}> = () => {
   };
 
   const theme = useTheme();
-  const isXsScreen = useMediaQuery(theme.breakpoints.only('xs'));
+  const isXsScreen = useMediaQuery(theme.breakpoints.only("xs"));
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
         position="fixed"
         sx={{
-          width: isXsScreen ? '100%' : '80%', // 100% ширина на экранах xs, 80% на остальных
-          left: '50%',
-          transform: 'translateX(-50%)',
-          zIndex: '54',
-          backgroundColor: 'white',
-          boxShadow: '0',
-          borderRadius: isXsScreen ? '0' : '0px 0px 20px 20px', // Без скругления на экранах xs
+          width: isXsScreen ? "100%" : "80%", // 100% ширина на экранах xs, 80% на остальных
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: "54",
+          backgroundColor: "white",
+          boxShadow: "0",
+          borderRadius: isXsScreen ? "0" : "0px 0px 20px 20px", // Без скругления на экранах xs
         }}
       >
         <Toolbar>
           <Typography component="div" sx={{ flexGrow: 1 }}>
-            <img src="/logos/header_logo.svg" alt="" style={{ width: isXsScreen ? '50%' : '30%', margin: '10px 0px' }} />
+            <img
+              src="/logos/header_logo.svg"
+              alt=""
+              style={{ width: isXsScreen ? "50%" : "30%", margin: "10px 0px" }}
+            />
           </Typography>
           {/* Навигационные кнопки для десктопа */}
-          <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center' }}>
+          <Box
+            sx={{ display: { xs: "none", sm: "flex" }, alignItems: "center" }}
+          >
             {menuItems.map((item, index) => (
               <Button
                 key={index}
                 color="inherit"
-                sx={{ color: 'black', fontSize: '14px', textTransform: 'none', padding: '8px 16px', whiteSpace: 'nowrap' }}
+                sx={{
+                  color: "black",
+                  fontSize: "14px",
+                  textTransform: "none",
+                  padding: "8px 16px",
+                  whiteSpace: "nowrap",
+                }}
                 onClick={item.onClick}
               >
                 {item.label}
@@ -84,32 +106,32 @@ const ButtonAppBar: React.FC<{}> = () => {
             ))}
           </Box>
           {/* Иконка меню для мобильных устройств */}
-          <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
+          <Box sx={{ display: { xs: "flex", sm: "none" } }}>
             <IconButton
               size="large"
               edge="start"
               color="inherit"
               aria-label="menu"
-              sx={{ mr: 2, color: 'black' }}
+              sx={{ mr: 2, color: "black" }}
               onClick={toggleMenu}
             >
               <MenuIcon />
             </IconButton>
           </Box>
-          <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
-            <PrimaryButton buttonText='Записаться на прием'></PrimaryButton>
+          <Box sx={{ display: { xs: "none", sm: "flex" } }}>
+            <PrimaryButton buttonText="Записаться на прием"></PrimaryButton>
           </Box>
           {/* Выпадающее меню на мобильных устройствах */}
           <Menu
             anchorEl={menuOpen ? anchorEl : null}
             anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
+              vertical: "top",
+              horizontal: "right",
             }}
             keepMounted
             transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
+              vertical: "top",
+              horizontal: "right",
             }}
             open={menuOpen}
             onClose={toggleMenu}
@@ -124,6 +146,6 @@ const ButtonAppBar: React.FC<{}> = () => {
       </AppBar>
     </Box>
   );
-}
+};
 
 export default ButtonAppBar;
