9d49f5ffacfe4273455dfbde9d011c2a8c8bf8be
suusumi <wer113wer@yandex.ru>
2023-09-28 12:14:01 +0300

    [fix](outline button): button borderRadius changed

--- src/components/outlineButton/OutlineButton.tsx
+++ src/components/outlineButton/OutlineButton.tsx
@@ -1,36 +1,39 @@
-import Button from '@mui/material/Button';
-import React from 'react';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import Button from "@mui/material/Button";
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 interface OutlineButtonProps {
   buttonText: string;
   buttonLink: string;
 }
 
-const OutlineButton: React.FC<OutlineButtonProps> = ({ buttonText, buttonLink }) => {
+const OutlineButton: React.FC<OutlineButtonProps> = ({
+  buttonText,
+  buttonLink,
+}) => {
   const theme = useTheme();
-  const isXsScreen = useMediaQuery(theme.breakpoints.only('xs'));
+  const isXsScreen = useMediaQuery(theme.breakpoints.only("xs"));
 
   return (
     <Button
       sx={{
-        color: 'black',
-        backgroundColor: 'transparent',
-        borderRadius: '10px',
-        fontSize: '14px',
-        textTransform: 'none',
-        padding: '8px 36px',
-        whiteSpace: 'nowrap',
-        border: '2px solid #288e81',
-        margin: isXsScreen ? '0 15px 15px 15px' : '0 15px 15px 0px',
-        '&:hover': {
-          backgroundColor: '#288e81',
-          color: 'white',
+        color: "black",
+        backgroundColor: "transparent",
+        borderRadius: "5px",
+        fontSize: "14px",
+        textTransform: "none",
+        padding: "8px 36px",
+        whiteSpace: "nowrap",
+        border: "2px solid #288e81",
+        margin: isXsScreen ? "0 15px 15px 15px" : "0 15px 15px 0px",
+        "&:hover": {
+          backgroundColor: "#288e81",
+          color: "white",
         },
       }}
       href={buttonLink}
-      target="_blank"   // Опционально, для открытия ссылки в новой вкладке
+      target="_blank" // Опционально, для открытия ссылки в новой вкладке
     >
       {buttonText}
     </Button>
