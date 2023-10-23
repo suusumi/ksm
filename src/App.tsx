0743b06ac7a00c3da39a877c24101681402d24c6
andreypisarev <xaandreynx@gmail.com>
2023-10-23 13:18:50 +0300

    [feat]<docs>: add page for Docs Clinic

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { SpecialistsScreen } from './pages/specialists/SpecialistsScreen';
 import { BannersScreen } from './pages/banners/BannersScreen';
 import AppointmentFormScreen from './pages/appointmentForm/AppointmentFormScreen';
 import PrivacyPolicyScreen from './pages/privacyPolicy/PrivacyPolicyScreen';
+import DocsScreen from './pages/docs/DocsScreen';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
           <Route path={routes.main} element={<MainPanelContainer children={<MainScreen />} />} />
           <Route path={routes.appointment} element={<MainPanelContainer children={<AppointmentFormScreen />}/>}/>
           <Route path={routes.privacyPolicy} element={<MainPanelContainer children={<PrivacyPolicyScreen />} />}/>
+          <Route path={routes.docs} element={<MainPanelContainer children={<DocsScreen />} />}/>
           <Route path={routes.authAdmin} element={<AdminLoginScreen />} />
           <Route path={routes.infographics} element={<AdminPanelContainer children={<InfographicsScreen />} />} />
           <Route path={routes.services} element={<AdminPanelContainer children={<ServicesScreen />} />} />
