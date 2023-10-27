f9679d510269003a7e88dd960cf9d9095808595c
andreypisarev <xaandreynx@gmail.com>
2023-10-27 14:24:39 +0300

    [feat]<services>: add dto for category

--- src/api/services/dto.ts
+++ src/api/services/dto.ts
@@ -1,18 +1,49 @@
 /**
- * GetServicesDto
+ * ServiceDto
  *
  * @property {number} id - идентификатор
- * @property {string} type - ServiceType
- * @property {string} category - CategoryType
  * @property {string} name - наименование услуги
  * @property {string} description - номер услуги
  * @property {number} price - цена
+ * @property {number} subCategoryId - идентификатор SubCategory
  */
-export type ServicesDto = {
+export type ServiceDto = {
   id: number;
-  type: string;
-  category: string;
   name: string;
   description: string;
   price: number;
+  subCategoryId: number;
 };
+
+/**
+ * SubCatrgotyDro
+ * 
+ * @property {number} id - идентификатор
+ * @property {string} name - намиенование подкатегории
+ * @property {categoryId} categoryId - иденнтификатор Category
+ */
+export type SubCategoryDto = {
+  id: number,
+  name: string,
+  categoryId: number
+}
+
+/**
+ * CategoryDto
+ * 
+ * @property {number} id - идентификатор
+ * @property {string} name - наименование категории
+ */
+export type CategoryDto = {
+  id: number,
+  name: string
+}
+
+/**
+ * Create Or Update Categoty Dto
+ * 
+ * @property {string} name - наимаенование категории
+ */
+export type CreateOrUpdateCategoryDto = {
+  name: string
+}
