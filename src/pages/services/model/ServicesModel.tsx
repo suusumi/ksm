3fa0ec62096a32ed32b63b5479830a55b947ca7c
andreypisarev <xaandreynx@gmail.com>
2023-10-30 15:56:01 +0300

    [feat]<services>: add servies Servies page

--- src/pages/services/model/ServicesModel.tsx
+++ src/pages/services/model/ServicesModel.tsx
@@ -1,22 +1,30 @@
-import { CategoryDto, CreateOrUpdateCategoryDto, SubcategoryDto } from "../../../api/services/dto";
+import { CategoryDto, CreateOrUpdateCategoryDto, CreateOrUpdateServiceDto, ServiceDto, SubcategoryDto } from "../../../api/services/dto";
 
 export interface ServicesViewProps {
   categories: CategoryDto[] | undefined;
   subcategories: SubcategoryDto[] | undefined;
+  services: ServiceDto[] | undefined;
   idButtonSelection: number;
   idChangeSubcategory: number;
+  idChangeService: number;
   isOpenChangeCategory: boolean;
   newCategory: CreateOrUpdateCategoryDto;
   newSubcategory: CreateOrUpdateCategoryDto;
+  newService: CreateOrUpdateServiceDto;
   handleChoise: Function;
   handleCreateCategory: Function;
   handleCreateSubcategory: Function;
+  handleCreateServise: Function;
   openFormChangeCategory: Function;
   openFormChangeSubcategory: Function;
+  openFormChangeServise: Function;
   handleChangeCategory: Function;
   handleChangeSubcategory: Function;
+  handleChangeService: Function;
   handleUpdateCategory: Function;
   handleUpdateSubcategory: Function;
+  handleUpdateService: Function;
   handleDeleteCategory: Function;
   handleDeleteSubcategory: Function;
+  handleDeleteService: Function;
 }
