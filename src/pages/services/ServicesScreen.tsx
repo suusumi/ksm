61becc3f3dec34314610d9d25a9cf29e3066cc82
andreypisarev <xaandreynx@gmail.com>
2023-10-30 12:51:36 +0300

    [feat]<services>: add subcategories Servies page

--- src/pages/services/ServicesScreen.tsx
+++ src/pages/services/ServicesScreen.tsx
@@ -2,21 +2,37 @@ import React, { useEffect, useState } from "react";
 import { ServicesView } from "./view/ServicesView";
 import {
   createCategory,
+  createSubcategory,
   deleteCategory,
+  deleteSubcategory,
   fetchAllCategories,
+  fetchAllSubcategories,
   updateCategory,
+  updateSubcategory,
 } from "../../api/services/request";
-import { CategoryDto, CreateOrUpdateCategoryDto } from "../../api/services/dto";
+import {
+  CategoryDto,
+  CreateOrUpdateCategoryDto,
+  CreateOrUpdateSubcategoryDto,
+  SubcategoryDto,
+} from "../../api/services/dto";
 
 export const ServicesScreen = () => {
   const [categories, setCategories] = useState<CategoryDto[]>();
-  const [idButtonSelection, setIdButtonSelection] = useState(-1);
+  const [subcategories, setSubcategories] = useState<SubcategoryDto[]>();
+  const [idButtonSelection, setIdButtonSelection] = useState<number>(-1);
+  const [idChangeSubcategory, setIdChangeSubcategory] = useState<number>(-1);
   const [update, setUpdate] = useState({});
   const [isOpenChangeCategory, setIsOpenChangeCategory] =
     useState<boolean>(false);
   const [newCategory, setNewCategory] = useState<CreateOrUpdateCategoryDto>({
     name: "",
   });
+  const [newSubcategory, setNewSubcategory] =
+    useState<CreateOrUpdateSubcategoryDto>({
+      name: "",
+      category_id: -1
+    });
 
   useEffect(() => {
     fetchAllCategories()
@@ -24,7 +40,15 @@ export const ServicesScreen = () => {
         setCategories(data);
       })
       .catch((error) => console.error(error));
-  }, [, update]);
+  }, [update]);
+
+  useEffect(() => {
+    fetchAllSubcategories()
+      .then((data) => {
+        setSubcategories(data);
+      })
+      .catch((error) => console.error(error));
+  }, [update]);
 
   const handleChoise = (id: number) => {
     setIdButtonSelection(id);
@@ -43,22 +67,61 @@ export const ServicesScreen = () => {
     setUpdate({});
   };
 
+  const handleCreateSubcategory = async () => {
+    const count = subcategories?.filter(
+      (subcategory) => subcategory.category_id === idButtonSelection
+    ).length;
+
+    const newSubcategory: CreateOrUpdateSubcategoryDto = {
+      name: `New Category ${
+        count === undefined || count === 0 ? 1 : count + 1
+      }`,
+      category_id: idButtonSelection
+    };
+    await createSubcategory(newSubcategory).catch((error) => console.error(error));
+    setUpdate({});
+  };
+
   const openFormChangeCategory = () => {
     setIsOpenChangeCategory(!isOpenChangeCategory);
+  };
+
+  const openFormChangeSubcategory = (id: number) => {
+    if (idChangeSubcategory === id) {
+      setIdChangeSubcategory(-1);
+    } else {
+      setIdChangeSubcategory(id);
+    }
   };
 
   useEffect(() => {
     if (isOpenChangeCategory) {
       setNewCategory({
         name:
-          categories?.find((category) => category.id == idButtonSelection)
+          categories?.find((category) => category.id === idButtonSelection)
             ?.name ?? "",
       });
     }
   }, [isOpenChangeCategory]);
 
+  useEffect(() => {
+    setNewSubcategory({
+      name:
+        subcategories?.find(
+          (subcategory) => subcategory.id === idChangeSubcategory
+        )?.name ?? "",
+        category_id: idButtonSelection
+    });
+  }, [idChangeSubcategory]);
+
   const handleChangeCategory = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewCategory({ name: event.target.value });
+  };
+
+  const handleChangeSubcategory = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setNewSubcategory({ name: event.target.value, category_id: idButtonSelection });
   };
 
   const handleUpdateCategory = async (id: number) => {
@@ -69,24 +132,45 @@ export const ServicesScreen = () => {
     setUpdate({});
   };
 
+  const handleUpdateSubcategory = async (id: number) => {
+    await updateSubcategory(id, newSubcategory).catch((error) =>
+      console.error(error)
+    );
+    openFormChangeSubcategory(id);
+    setUpdate({});
+  };
+
   const handleDeleteCategory = (id: number) => {
     deleteCategory(id).catch((error) => console.error(error));
     handleChoise(-1);
     setUpdate({});
   };
 
+  const handleDeleteSubcategory = (id: number) => {
+    deleteSubcategory(id).catch((error) => console.error(error));
+    setUpdate({});
+  };
+
   return (
     <ServicesView
       categories={categories}
+      subcategories={subcategories}
       idButtonSelection={idButtonSelection}
+      idChangeSubcategory={idChangeSubcategory}
       isOpenChangeCategory={isOpenChangeCategory}
       newCategory={newCategory}
+      newSubcategory={newSubcategory}
       handleChoise={handleChoise}
       handleCreateCategory={handleCreateCategory}
+      handleCreateSubcategory={handleCreateSubcategory}
       openFormChangeCategory={openFormChangeCategory}
+      openFormChangeSubcategory={openFormChangeSubcategory}
       handleChangeCategory={handleChangeCategory}
+      handleChangeSubcategory={handleChangeSubcategory}
       handleUpdateCategory={handleUpdateCategory}
+      handleUpdateSubcategory={handleUpdateSubcategory}
       handleDeleteCategory={handleDeleteCategory}
+      handleDeleteSubcategory={handleDeleteSubcategory}
     />
   );
 };
--- src/pages/services/model/ServicesModel.tsx
+++ src/pages/services/model/ServicesModel.tsx
@@ -1,14 +1,22 @@
-import { CategoryDto, CreateOrUpdateCategoryDto } from "../../../api/services/dto";
+import { CategoryDto, CreateOrUpdateCategoryDto, SubcategoryDto } from "../../../api/services/dto";
 
 export interface ServicesViewProps {
   categories: CategoryDto[] | undefined;
+  subcategories: SubcategoryDto[] | undefined;
   idButtonSelection: number;
+  idChangeSubcategory: number;
   isOpenChangeCategory: boolean;
   newCategory: CreateOrUpdateCategoryDto;
+  newSubcategory: CreateOrUpdateCategoryDto;
   handleChoise: Function;
   handleCreateCategory: Function;
+  handleCreateSubcategory: Function;
   openFormChangeCategory: Function;
+  openFormChangeSubcategory: Function;
   handleChangeCategory: Function;
+  handleChangeSubcategory: Function;
   handleUpdateCategory: Function;
+  handleUpdateSubcategory: Function;
   handleDeleteCategory: Function;
+  handleDeleteSubcategory: Function;
 }
