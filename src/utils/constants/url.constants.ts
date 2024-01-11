e67e4f802619af8864c49409a95f22d5e60ea2ee
suusumi <wer113wer@yandex.ru>
2024-01-11 16:18:17 +0300

    [fix[(cors error): correct communication with the back end on server

--- src/utils/constants/url.constants.ts
+++ src/utils/constants/url.constants.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://5.42.78.202:8081/";
-export const IMAGE_URL = "http://5.42.78.202:8081/images/";
+export const BACKEND_URL = "http://178.35.139.202:8081/";
+export const IMAGE_URL = "http://178.35.139.202:8081/images/";
