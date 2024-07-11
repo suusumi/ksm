a827b7f262f4840bc3d82d6eda06f724fd1db201
andreypisarev <xaandreynx@gmail.com>
2024-07-11 16:30:06 +0300

    [fix]: update url

--- src/utils/constants/url.constants.ts
+++ src/utils/constants/url.constants.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = "https://1clinic-backend.volgmed.ru/";
 export const IMAGE_URL = "https://1clinic-backend.volgmed.ru/images/";
-export const BACKEND_REGISTRATIONS_URL = "https://feedback.visdom.tech/api/v1/"
+export const BACKEND_REGISTRATIONS_URL = "https://1clinic-feedback.visdom.tech/api/v1/"
