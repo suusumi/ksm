818d59f9fc2b23745f77c88396a48503c35c09cb
andreypisarev <xaandreynx@gmail.com>
2024-07-11 16:28:31 +0300

    [fix]<client>:update url

--- src/utils/constants/url.constants.ts
+++ src/utils/constants/url.constants.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "https://zachem-mne-vse-eto.products.volgmed.ru/";
-export const IMAGE_URL = "https://zachem-mne-vse-eto.products.volgmed.ru/images/";
+export const BACKEND_URL = "https://1clinic-backend.volgmed.ru/";
+export const IMAGE_URL = "https://1clinic-backend.volgmed.ru/images/";
 export const BACKEND_REGISTRATIONS_URL = "https://feedback.visdom.tech/api/v1/"
