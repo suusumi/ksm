fb7d2b776f5cdf2b0682a0ca53e4b289cbe0767f
Sergey Groshev <wer113wer@yandex.ru>
2024-11-01 14:09:13 +0300

    [feat](yclient): create yclient form component

---
+++ src/components/yclient/yclient.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Yclient = () => {
+    return (
+        <iframe
+            height="745px"
+            width="100%"
+            // scrolling="no"
+            frameBorder="0"
+            // allowTransparency="true"
+            id="ms_booking_iframe"
+            src="https://n1267021.yclients.com/company/1128518/personal/menu?o="
+            style={{margin: 0}}
+        ></iframe>
+    );
+};
