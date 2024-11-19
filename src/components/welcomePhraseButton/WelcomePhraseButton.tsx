a77bef129e32b86cd188ca204728fc66a8b71cab
Sergey Groshev <wer113wer@yandex.ru>
2024-11-19 16:51:39 +0300

    [feat](welcome block button): different color for upper-button

--- src/components/welcomePhraseButton/WelcomePhraseButton.tsx
+++ src/components/welcomePhraseButton/WelcomePhraseButton.tsx
@@ -1,16 +1,31 @@
 import Button from '@mui/material/Button';
 import React from 'react'
+import {useTheme} from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 interface WelcomePhraseButtonProps {
     slogan: string;
 }
 
 const WelcomePhraseButton: React.FC<WelcomePhraseButtonProps> = (props) => {
-  return (
-        <Button color="primary" sx={{color: '#288e81', backgroundColor:'#white', border: '2px solid #288e81', borderRadius:'30px', fontSize: '15px', textTransform: 'none', padding: '8px 20px', display: { xs: 'flex', sm: 'none', lg: 'flex' }, whiteSpace: 'nowrap'}}>
+    const theme = useTheme();
+    const isXsScreen = useMediaQuery(theme.breakpoints.only("xs"));
+    return (
+        <Button color="primary" sx={{
+            color: isXsScreen ? '#288e81' : '#ffffff',
+            backgroundColor: '#white',
+            border: isXsScreen ? '1px solid #288e81' : '1px solid #ffffff',
 
-        {props.slogan}</Button>
-  )
+            borderRadius: '30px',
+            fontSize: '15px',
+            textTransform: 'none',
+            padding: '8px 20px',
+            display: {xs: 'flex', sm: 'none', lg: 'flex'},
+            whiteSpace: 'nowrap'
+        }}>
+
+            {props.slogan}</Button>
+    )
 }
 
 export default WelcomePhraseButton
