665ec80ea3203d061ffa170655684974cbf76f1c
Sergey Groshev <wer113wer@yandex.ru>
2024-12-09 17:00:15 +0300

    [feat](about us user-block): remove plug, get data from backend

--- src/components/aboutUsBlock/AboutUsBlock.tsx
+++ src/components/aboutUsBlock/AboutUsBlock.tsx
@@ -1,119 +1,115 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { ThemeProvider, useTheme } from "@mui/material/styles";
+import {useTheme} from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import PlacePhoto from "../../assets/content/main/aboutUsBlock/clinic_photo.png";
 import Grid from "@mui/material/Grid";
+import {fetchAboutUs} from "../../api/aboutUs/request";
+import {AboutUs} from "../../api/aboutUs/dto";
 
-// TODO: Необходимо передавать image (его url или придумать какой-то альтернативный вариант)
-interface AboutUsBlockData {
-  title: string;
-  text: string;
-}
-
-/**
- * Идентификатор для корректной навигации в шапке.
- * @interface
- *
- * @property {string} id Идентификатор блока.
- */
 interface AboutUsBlockProps {
-  id: string;
+    id: string;
 }
 
 /**
- * Компонент, отображающий блок о нас.
+ * Компонент, отображающий блок "О нас".
  *
  * @param {AboutUsBlockProps} id - Идентификатор для корректной навигации в шапке.
  * @return {ReactElement} Отрисованный компонент AboutUsBlock
  */
-const AboutUsBlock: React.FC<AboutUsBlockProps> = ({ id }) => {
-  const [data, setData] = useState<AboutUsBlockData[]>([]);
+const AboutUsBlock: React.FC<AboutUsBlockProps> = ({id}) => {
+    const [aboutUs, setAboutUs] = useState<AboutUs | null>(null); // Состояние для данных "О нас"
+    const [error, setError] = useState<string | null>(null); // Состояние для ошибки
 
-  useEffect(() => {
-    const aboutUsBlockData: AboutUsBlockData[] = [
-      {
-        title: "О нас",
-        text: "Клиника семейной медицины была создана в 2006 году в результате сотрудничества с Арканзасским университетом медицинских наук. Является структурным подразделением ВолгГМУ оказывающим амбулаторную медицинскую помощи для всех желающих. Прием ведут ведущие специалисты медицинского университета.",
-      },
-    ];
+    useEffect(() => {
+        const loadAboutUsData = async () => {
+            try {
+                const data = await fetchAboutUs(); // Загружаем данные
+                setAboutUs(data);
+            } catch (err) {
+                console.error("Ошибка загрузки данных 'О нас':", err);
+                setError("Не удалось загрузить данные. Попробуйте позже.");
+            }
+        };
 
-    setData(aboutUsBlockData);
-  }, []);
+        loadAboutUsData();
+    }, []);
 
-  const theme = useTheme();
-  const isXsScreen = useMediaQuery(theme.breakpoints.only("xs"));
+    const theme = useTheme();
+    const isXsScreen = useMediaQuery(theme.breakpoints.only("xs"));
 
-  const TitleText = {
-    color: "#288e81",
-    fontSize: isXsScreen ? 22 : 38,
-    fontFamily: "Austin, sans-serif",
-    textTransform: "uppercase",
-    maxWidth: isXsScreen ? 240 : 500,
-    textAlign: isXsScreen ? "center" : "right",
-    marginBottom: "25px",
-  };
+    const TitleText = {
+        color: "#288e81",
+        fontSize: isXsScreen ? 22 : 38,
+        fontFamily: "Austin, sans-serif",
+        textTransform: "uppercase",
+        maxWidth: isXsScreen ? 240 : 500,
+        textAlign: isXsScreen ? "center" : "right",
+        marginBottom: "25px",
+    };
 
-  const Text = {
-    fontSize: isXsScreen ? 16 : 18,
-    maxWidth: isXsScreen ? 300 : 500,
-    textAlign: isXsScreen ? "center" : "right",
-    display: "flex",
-  };
+    const Text = {
+        fontSize: isXsScreen ? 16 : 18,
+        maxWidth: isXsScreen ? 300 : 500,
+        textAlign: isXsScreen ? "center" : "right",
+        display: "flex",
+    };
 
-  return (
-    <div id={id} style={{ padding: "50px 0px" }}>
-      <Grid container spacing={5}>
-        {isXsScreen ? (
-          <Grid
-            item
-            xs={12}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-              position: "relative",
-            }}
-          >
-            <Typography sx={TitleText}>{data[0]?.title || ""}</Typography>
-            <Typography sx={Text}>{data[0]?.text || ""}</Typography>
-            <Box
-              component="img"
-              sx={{ width: "80%", paddingTop: "30px" }}
-              src={PlacePhoto}
-            ></Box>
-          </Grid>
-        ) : (
-          <>
-            <Grid item xs={12} sm={6} md={6}>
-              <Box
-                component="img"
-                sx={{ width: "100%" }}
-                alt="Здание Клиника семейной медицины"
-                src={PlacePhoto}
-              ></Box>
+    if (error) {
+        return (
+            <div id={id} style={{padding: "50px 0px", textAlign: "center"}}>
+                <Typography color="error">{error}</Typography>
+            </div>
+        );
+    }
+
+    return (
+        <div id={id} style={{padding: "50px 0px"}}>
+            <Grid container spacing={5}>
+                {isXsScreen ? (
+                    <Grid
+                        item
+                        xs={12}
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flexDirection: "column",
+                            position: "relative",
+                        }}
+                    >
+                        <Typography sx={TitleText}>О нас</Typography>
+                        <Typography sx={Text}>{aboutUs?.content || ""}</Typography>
+                        {aboutUs?.imageUrl && (
+                            <Box
+                                component="img"
+                                sx={{width: "80%", paddingTop: "30px", borderRadius:"15px"}}
+                                src={`http://localhost:8081/images/${aboutUs.imageUrl}`}
+                                alt="Фото клиники"
+                            ></Box>
+                        )}
+                    </Grid>
+                ) : (
+                    <>
+                        <Grid item xs={12} sm={6} md={6}>
+                            {aboutUs?.imageUrl && (
+                                <Box
+                                    component="img"
+                                    sx={{width: "100%", borderRadius:"15px"}}
+                                    alt="Фотография Клиники семейной медицины"
+                                    src={`http://localhost:8081/images/${aboutUs.imageUrl}`}
+                                ></Box>
+                            )}
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={6}>
+                            <Typography sx={TitleText}>О нас</Typography>
+                            <Typography sx={Text}>{aboutUs?.content || ""}</Typography>
+                        </Grid>
+                    </>
+                )}
             </Grid>
-            <Grid item xs={12} sm={6} md={6}>
-              <Typography sx={TitleText}>{data[0]?.title || ""}</Typography>
-              <Typography sx={Text}>{data[0]?.text || ""}</Typography>
-            </Grid>
-          </>
-        )}
-      </Grid>
-
-      {/* <Grid container spacing={5}>
-            <Grid item xs={12} sm={6} md={6}>
-                <Box component="img" sx={{ width: "100%" }} alt='Здание Клиника семейной медицины' src={PlacePhoto}></Box>
-            </Grid>
-            <Grid item xs={12} sm={6} md={6}>
-                <Typography sx={TitleText}>{data[0]?.title || ''}</Typography>
-                <Typography sx={Text}>{data[0]?.text || ''}</Typography>
-            </Grid>
-        </Grid> */}
-    </div>
-  );
+        </div>
+    );
 };
 
 export default AboutUsBlock;
